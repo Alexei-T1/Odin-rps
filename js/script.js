4b18174ba62815cc3f3dc120881840f9d66f34a2
Alexei-T1 <turencko.alexei@yandex.ru>
2021-07-13 14:24:35 +0400

    add: ui for rps; feature: 5 rounds to end

--- js/script.js
+++ js/script.js
@@ -3,71 +3,81 @@ const gameList = ["rock", "paper", "scissors"];
 function randomEnter(){
 
     const indexList = Math.floor(Math.random() * 3);
-    document.querySelector(".random_word").textContent = gameList[indexList];
+    document.querySelector("#random").textContent = gameList[indexList];
 
 return gameList[indexList];
-
 }
 
 function enter(event){
+    if (round == 0){
+        document.querySelector(".victory").style.display = 'none';
+    }
+
+    round++;
+    document.querySelector("#round").textContent = round;
+
     let userWord = event.target.textContent.toLowerCase();
-    document.querySelector(".user_word").textContent = userWord;
-
+    document.querySelector("#your").textContent = userWord;
     let randomWord = randomEnter();
-    console.log(userWord);
-    console.log(randomWord);
-    
-    let result = game(randomWord, userWord);
-    document.querySelector(".result").textContent = result;
-    console.log(result);
+    result += game(randomWord, userWord);
+    console.log(round, result)
+    document.querySelector("#points").textContent = result;
 
+    if (round == 5){
+        document.querySelector("#round").textContent = "end";
+        if (result > 0){
+            document.querySelector(".victory").textContent = "VICTORY!!!";
+            document.querySelector(".victory").style.display = 'block';
+        }
+        else if (result < 0){
+            document.querySelector(".victory").textContent = "LOSING!!!";
+            document.querySelector(".victory").style.display = 'block';
+        }
+        else{
+            document.querySelector(".victory").textContent = "DRAW!!!";
+            document.querySelector(".victory").style.display = 'block';
+        }
+
+        round = 0;
+        result = 0;
+        console.log(document.querySelector(".victory").style.display)
+        console.log(round, result)
+    }
 }
-
-
 
 function game (randomWord, userWord) {
 
     if (randomWord === userWord) {
-        return 'draw';
+        return 0;
     }
 
     else if (randomWord === "rock" && userWord === "paper") {
-        return 'You Win!';
+        return 1;
     }
     else if (randomWord === "rock" && userWord === "scissors") {
-        return 'You Lose!';
+        return -1;
     }
     else if (randomWord === "paper" && userWord === "scissors") {
-        return 'You Win!';
+        return 1;
     }
     else if (randomWord === "paper" && userWord === "rock") {
-        return 'You Lose!';
+        return -1;
     }
     else if (randomWord === "scissors" && userWord === "rock") {
-        return 'You Win!';
+        return 1;
     }
     else if (randomWord === "scissors" && userWord === "paper") {
-        return 'You Lose!';
+        return -1;
     }   
 }
+
+
+let result = 0;
+let round = 0;
+
 const buttons = document.querySelectorAll(".button")
-console.log(buttons);
 buttons.forEach((button) => {
     button.addEventListener('click', enter)
-}
-)
+});
 
 
-``` check = prompt("Once more, yes?").toLowerCase();
-
-while (['yes', 'yeh', "1"].indexOf(check) !== -1){
-
-    userWord = enter();
-    randomWord = randomEnter();
-    result = game(randomWord, userWord);
-    document.querySelector(".result").textContent = result;
-    console.log(result);
-   check = prompt("Once more, yes?").toLowerCase();
-} ```
-
-console.log("GoodBye!");
